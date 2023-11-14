c8e7625bd4a7a63ba4dc1eed906ab5e9d9ce3b41
Javod Mutalliboev <javodmutalliboev@gmail.com>
2023-11-14 20:56:49 +0500

    task in progress

---
+++ src/app/components/header.component.tsx
@@ -0,0 +1,25 @@
+"use client";
+import "../styles/header.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <div id="i1">
+      <div className={`link ${pathname === "/" ? "tab active" : "tab"}`}>
+        <Link href="/" as={""}>
+          Converter
+        </Link>
+      </div>
+      <div
+        className={`link ${pathname === "/currencies" ? "tab active" : "tab"}`}
+      >
+        <Link href="/currencies" as={"currencies"}>
+          Currencies
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/main.component.tsx
@@ -0,0 +1,9 @@
+import Header from "./header.component";
+
+export default function Main() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
